6597eb58591b6e9cceadb4c977e83547651e9371
ao-softserve <aosob@softserveinc.com>
2020-02-12 18:30:34 +0100

    Feature: Add conection provider component

---
+++ src/components/ConnectionInfo/ConnectionInfo.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Dialog, DialogTitle, DialogActions, Button, TextField } from "@material-ui/core";
+import { observer } from "mobx-react";
+import { StoreContext } from "../..";
+import { RootStore } from "../../stores";
+
+export const ConnectionInfo: React.FC = observer(() => {
+  const { common } = React.useContext<RootStore>(StoreContext);
+
+  const [ip, setIp] = React.useState("");
+  const [port, setPort] = React.useState("");
+
+  const handleIpChange = (e: any) => {
+    setIp(e.target.value);
+  };
+
+  const handlePortChange = (e: any) => {
+    setPort(e.target.value);
+  };
+
+  const handleOk = () => {
+    common.setIpAddress(ip);
+    common.setPort(port);
+  };
+  const canSubmit = ip && port;
+
+  const button = (
+    <Button onClick={handleOk} color="primary" disabled={!canSubmit} variant="contained">
+      Ok
+    </Button>
+  );
+
+  return (
+    <>
+      <Dialog disableBackdropClick disableEscapeKeyDown maxWidth="xs" aria-labelledby="choose-user-title" open={true}>
+        <DialogTitle id="choose-user-title">Provide Connections Info</DialogTitle>
+        <TextField value={ip} onChange={handleIpChange} label="Server IP" />
+        <TextField value={port} onChange={handlePortChange} label="Server Port" />
+
+        <DialogActions>{button}</DialogActions>
+      </Dialog>
+    </>
+  );
+});
--- src/index.js
+++ src/index.js
@@ -4,57 +4,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import ApolloClient from "apollo-client";
-import { WebSocketLink } from "apollo-link-ws";
-import { split } from "apollo-link";
-import { HttpLink } from "apollo-link-http";
-import { getMainDefinition } from "apollo-utilities";
 import { rootStore } from "./stores";
-
-const httpLink = new HttpLink({
-  uri: "http://localhost:5000/graphql"
-});
-
-const wsLink = new WebSocketLink({
-  uri: `ws://localhost:5000/graphql`,
-  options: {
-    reconnect: true
-  }
-});
-
-const link = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return definition.kind === "OperationDefinition" && definition.operation === "subscription";
-  },
-  wsLink,
-  httpLink
-);
-
-const GraphQLClient = new ApolloClient({
-  link,
-  cache: new InMemoryCache()
-});
-
-// const defaultValues: RootStore = {
-//   edua: {
-//     sim: [],
-//     simTime: 0
-//   },
-//   common: {
-//     userId: 1
-//   }
-// };
 
 export const StoreContext = React.createContext();
 
 ReactDOM.render(
   <StoreContext.Provider value={rootStore}>
-    <ApolloProvider client={GraphQLClient}>
-      <App />
-    </ApolloProvider>
+    <App />
   </StoreContext.Provider>,
   document.getElementById("root")
 );
